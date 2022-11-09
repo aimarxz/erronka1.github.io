26495ee55a59bd6f256d8563afff94b4ddc5b250
aimarxz <117787567+aimarxz@users.noreply.github.com>
2022-11-09 02:02:38 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,20 @@
+$(document).ready(function () {
+    console.log("rady :d")
+});
+
+
+$(document).on("scroll", function () {
+    var pageTop = $(document).scrollTop();
+    var pageBottom = pageTop + $(window).height();
+    var tags = $(".tag");
+
+    for (var i = 0; i < tags.length; i++) {
+        var tag = tags[i];
+
+        if ($(tag).position().top < pageBottom) {
+            $(tag).addClass("visible");
+        } else {
+            $(tag).removeClass("visible");
+        }
+    }
+});
